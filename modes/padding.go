8f1374fb266f41094c1711be8fb6f9035b3c5ac5
Jalaj <96870071+jalaj711@users.noreply.github.com>
2024-04-11 17:06:44 +0530

    added ISO padding
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

--- modes/padding.go
+++ modes/padding.go
@@ -6,6 +6,7 @@ type Padding interface {
 }
 
 type PKCS7 struct{}
+type ISO struct{}
 
 func (pk *PKCS7) Pad(data []byte, blockSize int) []byte {
 	size := len(data)
@@ -22,4 +23,28 @@ func (pk *PKCS7) UnPad(data []byte, blockSize int) []byte {
 		panic("invalid padding in data")
 	}
 	return data[:len(data)-int(data[len(data)-1])]
+}
+
+func (iso *ISO) Pad(data []byte, blockSize int) []byte {
+	size := len(data)
+	paddingBytes := blockSize - size%blockSize
+	if paddingBytes == 0 {
+		paddingBytes = 1
+	}
+	padding := make([]byte, paddingBytes)
+	padding[0] = 0x80
+	for i := 1; i < paddingBytes; i++ {
+		padding[i] = 0x0
+	}
+	return append(data, padding...)
+}
+
+func (iso *ISO) UnPad(data []byte, blockSize int) []byte {
+	if data[len(data)-1] != 0x0 && data[len(data)-1] != 0x80 {
+		panic("invalid padding in data")
+	}
+	i := len(data) - 1
+	for ; i >= 0 && data[i] != 0x80; i-- {
+	}
+	return data[:i]
 }
