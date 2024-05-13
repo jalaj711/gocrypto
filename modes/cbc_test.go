b5fbd97c252e19d5cd87d7a5bf98169e5bb8e72f
Jalaj <96870071+jalaj711@users.noreply.github.com>
2024-05-13 18:01:39 +0530

    added tests for cbc decryption
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

--- modes/cbc_test.go
+++ modes/cbc_test.go
@@ -22,7 +22,7 @@ func TestCBC_Encrypt(t *testing.T) {
 		want   []byte
 	}{
 		{
-			name: "cbc-aes-1",
+			name: "cbc-aes-enc-1",
 			fields: fields{
 				cipher:  &aes.AES{},
 				padding: &PKCS7{},
@@ -64,6 +64,69 @@ func TestCBC_Encrypt(t *testing.T) {
 			}, []byte{})
 			if got := cbc.Encrypt(tt.args.data); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Encrypt() = %x, want %x", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCBC_Decrypt(t *testing.T) {
+	type fields struct {
+		cipher  BlockCipher
+		padding Padding
+		iv      []byte
+	}
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   []byte
+	}{
+		{
+			name: "cbc-aes-dec-1",
+			fields: fields{
+				cipher:  &aes.AES{},
+				padding: &PKCS7{},
+				iv: []byte{
+					0x0, 0x0, 0x0, 0x0,
+					0x0, 0x0, 0x0, 0x0,
+					0x0, 0x0, 0x0, 0x0,
+					0x0, 0x0, 0x0, 0x0,
+				},
+			},
+			args: args{
+				data: []byte{
+					0xff, 0x46, 0x30, 0xf2,
+					0x5b, 0xbe, 0xb6, 0x3c,
+					0x7f, 0xcf, 0x45, 0x76,
+					0xf5, 0x0d, 0x4e, 0xa9,
+				},
+			},
+			want: []byte{
+				0x00, 0x7e, 0x65, 0x77,
+				0xe6, 0x57, 0x7a, 0xbc,
+				0x62, 0xcd, 0x28, 0x73,
+				0x65, 0xf1, 0x99,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cbc := &CBC{
+				cipher:  tt.fields.cipher,
+				padding: tt.fields.padding,
+				iv:      tt.fields.iv,
+			}
+			cbc.Init([]byte{
+				0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00,
+			}, []byte{})
+			if got := cbc.Decrypt(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Decrypt() = %x, want %x", got, tt.want)
 			}
 		})
 	}
