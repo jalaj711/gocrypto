8aaaae1e5b6dc88c78ce7f085424a57e08d8eee2
Jalaj <96870071+jalaj711@users.noreply.github.com>
2024-10-24 23:59:09 +0530

    feat: add padding method for sha512
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

---
+++ sha512/padding.go
@@ -0,0 +1,25 @@
+package sha512
+
+import "encoding/binary"
+
+func Pad(input []byte) []byte {
+	l := uint64(len(input))
+	toPad := l % 128
+	if toPad >= 112 {
+		toPad = 128 - toPad
+	} else {
+		toPad = 112 - toPad
+	}
+	input = append(input, 0b10000000)
+	toPad--
+	toPad += 8
+	for toPad > 0 {
+		input = append(input, 0)
+		toPad--
+	}
+	buf := make([]byte, 8)
+	// we want the output in big endian format
+	binary.BigEndian.PutUint64(buf, l<<3)
+	input = append(input, buf...)
+	return input
+}
